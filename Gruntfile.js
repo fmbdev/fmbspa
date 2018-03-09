5dffc3a4af8e9c71fe23082da4022aa402ea0ce0
Alexander García <halexgs1987@gmail.com>
2018-03-08 20:44:24 -0600

    Commit inicial del Frontend spa

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt){
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Sass Task
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'none'
+                },
+                files: {
+                  'src/compiled_styles/style.css': 'src/styles.scss'  
+                }
+            },
+            prod: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none'
+                },
+                files: {
+                    'src/compiled_styles/style-min.css': 'src/styles.scss'
+                }
+            }
+        },
+        // Watch Task
+        watch: {
+            css: {
+                files: '/src/app/**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask ('default', ['watch']);
+}
