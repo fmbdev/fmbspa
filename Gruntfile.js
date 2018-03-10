cd51c3e2303806f0d748fdde5c87555ff0521c03
Alexander García <halexgs1987@gmail.com>
2018-03-09 23:10:24 -0600

    Se arreglo el bug que tenia en grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function(grunt){
         // Watch Task
         watch: {
             css: {
-                files: '/src/app/**/*.scss',
+                files: 'src/app/**/*.scss',
                 tasks: ['sass']
             }
         }
