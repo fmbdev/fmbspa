52cb11feec8aa376827dba54eb4183b0cf48e612
Damian Ruiz <damian.ruiz@colorcodestudio.com.mx>
2018-04-06 19:34:52 -0400

    Update last validation

--- src/app/validations/landing.validations.ts
+++ src/app/validations/landing.validations.ts
@@ -6,20 +6,12 @@ export class LandingValidation {
   public palabraMal:string;
 
   constructor(){ }
-  /* static emailExistValidator(landingService: LandingService) {
-    return (control: AbstractControl) => {
-      return landingService.checkPalabra(control.value).map(res => {
-        return res ? null : { emailTaken: true };
-      });
-    };
-  }*/
 
   static palabraMalaValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } => {
       const name = control.value;
       let array = localStorage.getBasuraObs;
-      if (name=="") {        
-        const texto = "orale";        
+      if (name=="") {                     
         return { 'palabraMala': { name } }
       }else if (array.indexOf(name)>-1) {
         return { 'palabraMala': { name } }
