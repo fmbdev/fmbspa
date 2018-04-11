73494e34b9e66a11d349a152ec98df5a379b41bb
Alexander García <halexgs1987@gmail.com>
2018-04-10 19:48:41 -0500

    Actualizando mi rama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,15 @@ import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule,
 import { MatTableModule, MatDatepickerModule, MatNativeDateModule, MatDialogModule, MatRadioModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home.component';
-import { DialogComponent } from './dialog/dialog.component';
-import { NuevoRegistroComponent } from './nuevo-registro/nuevo-registro.component';
-import { NuevoRegistroSolovinoComponent } from './nuevo-registro-solovino/nuevo-registro-solovino.component';
-import { GeneralComponent } from './general/general.component';
-import { ReferidosComponent } from './referidos-referente/general.component';
-import { ReferidosPromotorComponent } from './referidos-promotor/referidos-promotor.component';
-import { ReferidosTlmkComponent } from './referidos-tlmk/referidos-tlmk.component';
-import { ReferidosWebComponent } from './referidos-web/referidos-web.component';
+import { NewRegisterPromotionComponent } from './new-register-promotion/new-register-promotion.component';
+import { SearchComponent } from './search/search.component';
+import { UploadBaseComponent } from './upload-base/upload-base.component';
+import { UploadBaseSisComponent } from './upload-base-sis/upload-base-sis.component';
+import { LoginComponent } from './login/login.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { NewRegisterComponent } from './new-register/new-register.component';
+import { NewRegisterSoloComponent } from './new-register-solo/new-register-solo.component';
+import { SearchInboundComponent } from './search-inbound/search-inbound.component';
 
 // Providers
 import { CsqService } from './providers/csq.service';
@@ -53,51 +52,8 @@ import { TipoReferenteService } from './providers/tipo-referente.service';
 import { EscuelaEmpresaService } from './providers/escuela-empresa.service';
 import { CitaProspeccionService } from './providers/cita-prospeccion.service';
 import { SubTipoActividadService } from './providers/sub-tipo-actividad.service';
-=======
-import { NewRegisterPromotionComponent } from './new-register-promotion/new-register-promotion.component';
-import { SearchComponent } from './search/search.component';
-import { UploadBaseComponent } from './upload-base/upload-base.component';
-import { UploadBaseSisComponent } from './upload-base-sis/upload-base-sis.component';
-import { LoginComponent } from './login/login.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { NewRegisterComponent } from './new-register/new-register.component';
-import { NewRegisterSoloComponent } from './new-register-solo/new-register-solo.component';
-import { SearchInboundComponent } from './search-inbound/search-inbound.component';
-
 import { GeneralService } from './services/general.service';
 import { LandingService } from './services/landing.service';
-    // Providers
-    import { CsqService } from './providers/csq.service';
-    import { PnnService } from './providers/pnn.service'; 
-    import { HoraService } from './providers/hora.service';
-    import { SendService } from './providers/send.service';
-    import { EquiService } from './providers/equi.service';
-    import { NivelService } from './providers/nivel.service';
-    import { CanalService } from './providers/canal.service';
-    import { CicloService } from './providers/ciclo.service';
-    import { TurnoService } from './providers/turno.service';
-    import { CampusService } from './providers/campus.service';
-    import { AsesorService } from './providers/asesor.service';
-    import { GeneroService } from './providers/genero.service';
-    import { InteresService } from './providers/interes.service';
-    import { CarreraService } from './providers/carrera.service';
-    import { UsuarioService } from './providers/usuario.service';
-    import { ModalidadService } from './providers/modalidad.service';
-    import { SinCorreoService } from './providers/sin-correo.service';
-    import { ParentescoService } from './providers/parentesco.service';
-    import { TerritorioService } from './providers/territorio.service';
-    import { CampusCitaService } from './providers/campus-cita.service';
-    import { ValidationService } from './validations/validation.service'; 
-    import { PalabramalaService } from './providers/palabramala.service';
-    import { ProgramacionService } from './providers/programacion.service';
-    import { TipificacionService } from './providers/tipificacion.service';
-    import { TransferenciaService } from './providers/transferencia.service';
-    import { PaginaLandingService } from './providers/pagina-landing.service';
-    import { TipoActividadService } from './providers/tipo-actividad.service';
-    import { TipoReferenteService } from './providers/tipo-referente.service';
-    import { EscuelaEmpresaService } from './providers/escuela-empresa.service';
-    import { CitaProspeccionService } from './providers/cita-prospeccion.service';
-    import { SubTipoActividadService } from './providers/sub-tipo-actividad.service';
 
 import { AppConfig } from './services/constants';
 import { ModalConfirmComponent } from './modal-confirm/modal-confirm.component';
@@ -109,7 +65,6 @@ import { ReferidoWebComponent } from './referido-web/referido-web.component';
 import { NewRegisterExistingComponent } from './new-register-existing/new-register-existing.component';
 import { NewRegisterExistingReceptionComponent } from './new-register-existing-reception/new-register-existing-reception.component';
 import { FormComponent } from './form/form.component';
->>>>>>> developer
 
 
 @NgModule({
@@ -126,19 +81,6 @@ import { FormComponent } from './form/form.component';
     NewRegisterSoloComponent,
     ModalConfirmComponent,
     HomeComponent,
-<<<<<<< HEAD
-    NuevoRegistroComponent,
-    NuevoRegistroSolovinoComponent,
-    GeneralComponent,
-    ReferidosComponent,
-    ReferidosPromotorComponent,
-    ReferidosTlmkComponent,
-    ReferidosWebComponent,
-    DialogComponent
-  ],
-  entryComponents: [
-    DialogComponent
-=======
     ReferidoReferenteComponent,
     ReferidoPromotorComponent,
     ReferidoTlmkComponent,
@@ -146,7 +88,6 @@ import { FormComponent } from './form/form.component';
     NewRegisterExistingComponent,
     NewRegisterExistingReceptionComponent,
     FormComponent
->>>>>>> developer
   ],
   imports: [
     BrowserModule,
@@ -172,11 +113,7 @@ import { FormComponent } from './form/form.component';
     CarreraService, CicloService, AsesorService, HoraService, PalabramalaService, PnnService, SendService, 
     CampusCitaService, CitaProspeccionService, EscuelaEmpresaService, GeneroService, PaginaLandingService,
     ProgramacionService, SinCorreoService, SubTipoActividadService, TerritorioService, TipoActividadService,
-<<<<<<< HEAD
-    TipoReferenteService, TransferenciaService, TurnoService, UsuarioService],
-=======
     TipoReferenteService, TransferenciaService, TurnoService, UsuarioService, AppConfig, GeneralService, LandingService],
->>>>>>> developer
   bootstrap: [AppComponent]
 })
 export class AppModule { }
