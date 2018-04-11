562d1b81736a0a2ecb36ad2f6ae2458eaffb266e
Alexander García <halexgs1987@gmail.com>
2018-04-10 19:22:01 -0500

    Agregando servicios restantes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule,
 import { MatTableModule, MatDatepickerModule, MatNativeDateModule, MatDialogModule, MatRadioModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
 import { DialogComponent } from './dialog/dialog.component';
 import { NuevoRegistroComponent } from './nuevo-registro/nuevo-registro.component';
@@ -53,31 +52,6 @@ import { TipoReferenteService } from './providers/tipo-referente.service';
 import { EscuelaEmpresaService } from './providers/escuela-empresa.service';
 import { CitaProspeccionService } from './providers/cita-prospeccion.service';
 import { SubTipoActividadService } from './providers/sub-tipo-actividad.service';
-=======
-import { NewRegisterPromotionComponent } from './new-register-promotion/new-register-promotion.component';
-import { SearchComponent } from './search/search.component';
-import { UploadBaseComponent } from './upload-base/upload-base.component';
-import { UploadBaseSisComponent } from './upload-base-sis/upload-base-sis.component';
-import { LoginComponent } from './login/login.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { NewRegisterComponent } from './new-register/new-register.component';
-import { NewRegisterSoloComponent } from './new-register-solo/new-register-solo.component';
-import { SearchInboundComponent } from './search-inbound/search-inbound.component';
-
-import { GeneralService } from './services/general.service';
-import { LandingService } from './services/landing.service';
-
-import { AppConfig } from './services/constants';
-import { ModalConfirmComponent } from './modal-confirm/modal-confirm.component';
-import { HomeComponent } from './home/home.component';
-import { ReferidoReferenteComponent } from './referido-referente/referido-referente.component';
-import { ReferidoPromotorComponent } from './referido-promotor/referido-promotor.component';
-import { ReferidoTlmkComponent } from './referido-tlmk/referido-tlmk.component';
-import { ReferidoWebComponent } from './referido-web/referido-web.component';
-import { NewRegisterExistingComponent } from './new-register-existing/new-register-existing.component';
-import { NewRegisterExistingReceptionComponent } from './new-register-existing-reception/new-register-existing-reception.component';
-import { FormComponent } from './form/form.component';
->>>>>>> developer
 
 
 @NgModule({
@@ -94,7 +68,6 @@ import { FormComponent } from './form/form.component';
     NewRegisterSoloComponent,
     ModalConfirmComponent,
     HomeComponent,
-<<<<<<< HEAD
     NuevoRegistroComponent,
     NuevoRegistroSolovinoComponent,
     GeneralComponent,
@@ -106,15 +79,6 @@ import { FormComponent } from './form/form.component';
   ],
   entryComponents: [
     DialogComponent
-=======
-    ReferidoReferenteComponent,
-    ReferidoPromotorComponent,
-    ReferidoTlmkComponent,
-    ReferidoWebComponent,
-    NewRegisterExistingComponent,
-    NewRegisterExistingReceptionComponent,
-    FormComponent
->>>>>>> developer
   ],
   imports: [
     BrowserModule,
@@ -135,16 +99,12 @@ import { FormComponent } from './form/form.component';
     MatDialogModule,
     MatRadioModule
   ],
-<<<<<<< HEAD
   providers: [CanalService, CsqService, TipificacionService, InteresService,
     ParentescoService, ValidationService, CampusService,EquiService, NivelService, ModalidadService,
     CarreraService, CicloService, AsesorService, HoraService, PalabramalaService, PnnService, SendService, 
     CampusCitaService, CitaProspeccionService, EscuelaEmpresaService, GeneroService, PaginaLandingService,
     ProgramacionService, SinCorreoService, SubTipoActividadService, TerritorioService, TipoActividadService,
     TipoReferenteService, TransferenciaService, TurnoService, UsuarioService],
-=======
-  providers: [AppConfig, GeneralService, LandingService],
->>>>>>> developer
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/providers/ciclo.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response} from '@angular/http';
+import { Ciclo } from '../interfaces/ciclo';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class CicloService {
+
+  private headers = new Headers({'Content-Type':'application/json'});
+
+  constructor(private http: Http) { }
+
+  getAll(): Observable<Ciclo[]>{
+    return this.http.get("http://devmx.com.mx/fmbapp/public/api/ciclo", {headers: this.headers})
+               .map(
+                 (res: Response) => res.json()
+               )
+  }
+
+}
