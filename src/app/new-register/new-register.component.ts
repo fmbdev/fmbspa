202b250f3fb696dec1c09a1c9bf571386b61c51b
Alexander García <halexgs1987@gmail.com>
2018-04-16 22:33:29 -0500

    Actualizando campos de lso formularios en landings page.

--- src/app/new-register/new-register.component.ts
+++ src/app/new-register/new-register.component.ts
@@ -261,7 +261,8 @@ export class NewRegisterComponent implements OnInit {
 
     onSubmit(){
      this.onKeyFechaNacimiento();
-     this.formatServ.changeFormatFechaCita(this.form.controls['FechaCita'].value);
+     let fecha_cita = this.formatServ.changeFormatFechaCita(this.form.controls['FechaCita'].value);
+     this.form.controls['FechaCita'].setValue(fecha_cita);
 
      this.sendServ.sendDataToApi(this.form.value)
          .subscribe(
