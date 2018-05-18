85dde54dd2d3d7b2ed87c6c2e3cdd40e9381ba48
Damian Ruiz <damian.ruiz@colorcodestudio.com.mx>
2018-05-18 10:07:38 -0400

    Actualizacion de performance

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -8,7 +8,7 @@ import  {MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class DialogComponent{
 
-  private message: string = "";
+  message: string = "";
   
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
