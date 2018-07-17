67d1c8ef2d4971c72cd0a58c051df5c5fb2ad7d2
mickdilon <ingmiguelangelgc@live.com>
2018-07-17 11:35:04 -0500

    Revision de ciclo e funcionalidad de interface ciclo, solo revision

--- src/app/interfaces/ciclo.ts
+++ src/app/interfaces/ciclo.ts
@@ -2,8 +2,10 @@ export interface Ciclo {
     Id: number;
     crmit_name: string;
     crmit_ciclovigenteventas: string;
+    crmit_codigounico: string;
     Activo: string;
     nombreventas: string;
     nombremes: string;
+    
 
 }
