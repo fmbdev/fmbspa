750841239e89357229b636b9758fc16abff9e3d6
mickdilon <ingmiguelangelgc@live.com>
2018-07-17 11:14:12 -0500

    Se actualizo Json de assets y src/assets de ciclo, tambien se actualizo el combolist de inbound y soloivino

--- src/app/interfaces/ciclo.ts
+++ src/app/interfaces/ciclo.ts
@@ -1,6 +1,9 @@
 export interface Ciclo {
     Id: number;
     crmit_name: string;
-    crmit_codigounico: string;
     crmit_ciclovigenteventas: string;
+    Activo: string;
+    nombreventas: string;
+    nombremes: string;
+
 }
